443e1fb62bb8dc04deba12f6a60f1ea3c6897ef3
clayton-andrade <clayton.andrade2009@gmail.com>
2021-10-01 08:54:07 -0300

    Alterações em minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,13 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Self, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Self, &'static str> {
+        args.next();
 
-        let query = args[1].to_owned();
-        let filename = args[2].to_owned();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -40,15 +44,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|&line| line.contains(query))
-        .collect::<Vec<&'a str>>()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|&line| line.to_lowercase().contains(&query.to_lowercase()))
-        .collect::<Vec<&'a str>>()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 
